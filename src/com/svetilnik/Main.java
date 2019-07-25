a6972fd443aa3085ff08daf1c60d8a890374b122
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-07-25 14:41:51 +0300

    Необходимо пересмотреть вторую задачу

--- src/com/svetilnik/Main.java
+++ src/com/svetilnik/Main.java
@@ -2,16 +2,20 @@ package com.svetilnik;
 
 import java.util.Scanner;
 
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 public class Main {
+    public static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        task1();
+//        task1();
+        tast2();
+        in.close();
     }
 
     public static void task1() {
         int a, b, c, z;
-
-        Scanner in = new Scanner(System.in);
 
         System.out.println("Enter a: ");
         a = in.nextInt();
@@ -25,6 +29,27 @@ public class Main {
         z = ((a - 3) * b / 2) + c;
 
         System.out.println("Rezult:" + z);
-        in.close();
+    }
+
+    //пересмотреть решение
+    public static void tast2() {
+        double a, b, c, Z;
+
+//        System.out.println("Enter a: ");
+//        a = in.nextInt();
+//
+//        System.out.println("Enter b: ");
+//        b = in.nextInt();
+//
+//        System.out.println("Enter c: ");
+//        c = in.nextInt();
+
+        a=1.0;
+        b=1.0;
+        c=1.0;
+
+        Z = ((b + sqrt(pow(b,2) + 4 * a *c)) / 2 * a) - (pow(a, 3) * c + pow(b, -2));
+
+        System.out.println("Result 2: "+ Z );
     }
 }
