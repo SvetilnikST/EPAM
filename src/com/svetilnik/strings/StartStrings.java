e226680597a811953756d39cb15d03e41e8233e9
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-08-15 20:51:41 +0300

    strings 2 task

--- src/com/svetilnik/strings/StartStrings.java
+++ src/com/svetilnik/strings/StartStrings.java
@@ -3,8 +3,9 @@ package com.svetilnik.strings;
 public class StartStrings {
     public static void startStrings(){
 
-        StringsAsArray.stringsAsArrayTask1();
+//        StringsAsArray.stringsAsArrayTask1();
 //        StringsAsArray.stringsAsArrayTask5("   nkjnj  njknjn njknjkn ");
 //        StringsAsArray.stringsAsArrayTask2();
+        StringsAsArray.stringsAsArrayTask6();
     }
 }
--- src/com/svetilnik/strings/StringsAsArray.java
+++ src/com/svetilnik/strings/StringsAsArray.java
@@ -66,8 +66,27 @@ public class StringsAsArray {
             System.out.print(strings[i] + " ");
         }
 
-
     }
+
+    public static void stringsAsArrayTask6() {
+        String[] str = new String[]{"1235648 dvfvdf1245 dl,;ls"};
+        int count = 0;
+
+        for (int i = 0; i < str.length; i++) {
+            char[] chars = str[i].toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+
+                if (chars[j] >= '1' && chars[j] <= '9') {
+                    count++;
+                }
+                System.out.print(chars[j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Coutnt number = " + count);
+    }
+
 
     //неправильно. нужно как с массивом символов
     public static void stringsAsArrayTask5(String s) {
