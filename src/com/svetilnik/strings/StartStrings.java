8e64f4a81d3df90fdbb9da46a02818288442c307
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-08-15 16:16:06 +0300

    Strings 1 Task Done

--- src/com/svetilnik/strings/StartStrings.java
+++ src/com/svetilnik/strings/StartStrings.java
@@ -3,8 +3,8 @@ package com.svetilnik.strings;
 public class StartStrings {
     public static void startStrings(){
 
-//        StringsAsArray.stringsAsArrayTask1();
+        StringsAsArray.stringsAsArrayTask1();
 //        StringsAsArray.stringsAsArrayTask5("   nkjnj  njknjn njknjkn ");
-        StringsAsArray.stringsAsArrayTask2();
+//        StringsAsArray.stringsAsArrayTask2();
     }
 }
--- src/com/svetilnik/strings/StringsAsArray.java
+++ src/com/svetilnik/strings/StringsAsArray.java
@@ -2,39 +2,69 @@ package com.svetilnik.strings;
 
 public class StringsAsArray {
 
-    public static void stringsAsArrayTask1(){
+    public static void stringsAsArrayTask1() {
 
+        String[] camelCase = new String[]{"karaMara", "helloWorld", "lizaMop", "optRew"};
+        String[] snake_case = new String[camelCase.length];
 
-        String str = "camelCase";
-        str = str.toLowerCase();
+        int codeSymbol = 95;
+        int cons = 32;
+        int asciiFrom = 40;
+        int asciiDo = 91;
 
-        System.out.println(str);
+        for (int j = 0; j < camelCase.length; j++) {
+
+            char[] result = camelCase[j].toCharArray();
+            char[] newrez = new char[result.length + 1];
+
+            int k = 0;
+            for (int i = 0; i < result.length; i++) {
+                if (result[i] > asciiFrom && result[i] < asciiDo) {
+                    newrez[k] = (char) ((int) codeSymbol);
+                    newrez[k + 1] = (char) ((int) result[i] + cons);
+                    k = k + 1;
+                } else
+                    newrez[k] = result[i];
+                k++;
+            }
+            String str = new String(newrez);
+            snake_case[j] = str;
+        }
+
+        System.out.println("camelCase: ");
+        for (String sCase : camelCase) {
+            System.out.print(sCase + " ");
+        }
+
+        System.out.println();
+        System.out.println("snake_case: ");
+        for (String aSnake : snake_case) {
+            System.out.print(aSnake + " ");
+        }
 
     }
 
 
-    public static void stringsAsArrayTask2(){
+    public static void stringsAsArrayTask2() {
 //        String str = "Starts word replace word word.";
 //        String forReplace = "word";
 //        String replacementValue = "letter";
 //        System.out.println(str);
 //        System.out.println(str.replace(forReplace, replacementValue));
 
-       String  [] strings = new String[]{"Start", "word", "replace", "word", "word"};
+        String[] strings = new String[]{"Start", "word", "replace", "word", "word"};
         String forReplace = "word";
         String replacementValue = "letter";
         for (int i = 0; i < strings.length; i++) {
-            if(strings[i]==forReplace){
-                strings[i]=replacementValue;
+            if (strings[i] == forReplace) {
+                strings[i] = replacementValue;
 
             }
         }
 
         for (int i = 0; i < strings.length; i++) {
-            System.out.print(strings[i]+" ");
+            System.out.print(strings[i] + " ");
         }
-
-
 
 
     }
