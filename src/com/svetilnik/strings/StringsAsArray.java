2a933e5dbfaa1c86196d2919d9cd8808f79b39bc
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-09-04 20:16:21 +0300

    voids Task2 - Refactoring

--- src/com/svetilnik/strings/StringsAsArray.java
+++ src/com/svetilnik/strings/StringsAsArray.java
@@ -47,235 +47,95 @@ public class StringsAsArray {
 
     }
 
-//    public static void stringsAsArrayTask2() {
-////        String str = "Starts word replace word word.";
-////        String forReplace = "word";
-////        String replacementValue = "letter";
-////        System.out.println(str);
-////        System.out.println(str.replace(forReplace, replacementValue));
-//
-////        String[] strings = new String[]{"Start", "word", "replace", "word", "word"};
-////        String forReplace = "word";
-////        String replacementValue = "letter";
-////        for (int i = 0; i < strings.length; i++) {
-////            if (strings[i] == forReplace) {
-////                strings[i] = replacementValue;
-////
-////            }
-////        }
-////
-////        for (int i = 0; i < strings.length; i++) {
-////            System.out.print(strings[i] + " ");
-////        }
-//
-//        String string = "String word word jkl word";
-//
-//        System.out.println(string.contains("word"));
-//
-////        char[] chars = {'w', 'o', 'r', 'd'};
-////        char[] latter = {'l', 'a', 't', 't', 'e', 'r'};
-//        char[] charsString = string.toCharArray();
-//        for (char c : charsString) {
-//            System.out.print(c);
-//        }
-//
-//    }
-
-//    public static void stringsAsArrayTask2(String string, String wordS, String replace) {
-//        char[] chars = string.toCharArray();
-//        char[] word = wordS.toCharArray();
-//        char[] letter = replace.toCharArray();
-//
-//        int count = 0;
-//        int k = 0;
-//        int z = 0;
-//
-//        for (int i = 0; i < chars.length; i++) {
-//            if (chars[i] == word[k]) {
-//                for (int j = 0; j < word.length; j++) {
-//                    if (chars[i + j] == word[j]) {
-//                        z++;
-//                    }
-//                    if (z == word.length) {
-//                        z = 0;
-//                        count++;
-//                    }
-//                }
-//
-//            }
-//        }
-//
-//        //вот тут рабоать с массивом
-//        int lengh = chars.length + ((letter.length - word.length) * count);
-//
-//
-//        char[] mass = new char[lengh];
-////        count = 0;
-//        z = 0;
-//
-//        for (int p = 0; p < mass.length; ) {
-//
-//            for (int i = 0; i < chars.length; i++) {
-//
-//                if (chars[i] == word[k]) {
-//                    for (int j = 0; j < word.length; j++) {
-//
-//                        if (chars[i + j] == word[j] && i + j < chars.length) {
-//                            z++;
-//                        }
-//
-//                        if (z == word.length) {
-//                            for (int l = 0; l < letter.length; l++) {
-//                                if (p + l < mass.length - 1) {
-//                                    mass[p + l] = letter[l];
-//                                    z = 0;
-//                                }
-//                            }
-//                            p = p + letter.length;
-//                            i = i + word.length - 1;
-////                            count++;
-//                        }
-//                    }
-//
-//                } else {
-//                    mass[p] = chars[i];
-//                    p++;
-//                }
-//
-//            }
-//        }
-//
-//
-//        for (int i = 0; i < chars.length; i++) {
-//            System.out.print(chars[i]);
-//        }
-//        System.out.println();
-//
-//        for (int i = 0; i < mass.length; i++) {
-//            System.out.print(mass[i]);
-//        }
-//
-//    }
-
-
-    public static void stringsAsArrayTask2(String string, String wordS, String replace) {
+    public static void stringsAsArrayTask2(String string, String replaceableWord, String wordToReplace) {
         char[] chars = string.toCharArray();
-        char[] word = wordS.toCharArray();
-        char[] letter = replace.toCharArray();
+        char[] word = replaceableWord.toCharArray();
+        char[] letter = wordToReplace.toCharArray();
+        char[] mass;
+        int lengh;
+        int count;
 
-        int count = 0;
-        int k = 0;
+        count = countRepeat(chars, word);
+        lengh = lengthArray(chars, word, letter, count);
+        mass = new char[lengh];
+        replaceWord(chars, word, letter, mass);
+        printChars(chars);
+        printChars(mass);
+    }
+
+    private static void printChars(char[] chars) {
+        for (int i = 0; i < chars.length; i++) {
+            System.out.print(chars[i]);
+        }
+        System.out.println();
+    }
+
+    private static int countRepeat(char[] chars, char[] word) {
+
         int z = 0;
-
-//        for (int i = 0; i < chars.length; i++) {
-//            if (chars[i] == word[k] && i != chars.length - 1) {
-//
-//                for (int j = 0; j < word.length; j++) {
-//                    if (i + j < chars.length - 1 && chars[i + j] == word[j]) {
-////                        if (chars[i + j] == word[j]) {
-//                        z++;
-//                        if (z == word.length) {
-//                            count++;
-//                            z = 0;
-//                        } else {
-//                            z = 0;
-//                            continue;
-//                        }
-////                        } else {
-////                            z = 0;
-////                            continue;
-////                        }
-//                    }
-//                }
-//            }
-//        }
+        int count = 0;
 
         for (int i = 0; i < chars.length; ) {
 
-            //если первый элемент начального массива = w
             if (chars[i] == word[0] && i != chars.length - 1) {
 
-                //проходим по массиву "word"
                 for (int j = 0; j < word.length; j++) {
 
-                    //если i элемент = j элементу в слове word
                     if (chars[i + j] == word[j] && i + j < chars.length) {
-                        //считаем количество совпадений
                         z++;
                     }
                 }
-                //после прохождения по циклу ворд - начинаем проверять сколько раз встречалось
-                //если количество совпадений равно количеству букв в слове word
+
                 if (z == word.length) {
-                    //обнуляем счетчик
                     z = 0;
-                    //проходимся по массиву letter
                     count++;
-                    //переходим к следующему i за словом word
                     i = i + word.length;
                 } else {
-
-                    if (i < chars.length) {
-                        i++;
-                        z = 0;
-                    }
+                    i++;
+                    z = 0;
                 }
             } else {
-                if (i < chars.length) {
-                    i++;
-                }
+                i++;
             }
         }
+        return count;
+    }
 
+    private static void replaceWord(char[] chars, char[] word, char[] letter, char[] mass) {
 
-        z = 0;
+        int z = 0;
 
-
-        int lengh = chars.length + ((letter.length - word.length) * (count));
-        char[] mass = new char[lengh];
-
-        //начинаем заполнять новый массив
         for (int p = 0; p < mass.length; ) {
-
-            //проходим по массиву изначальному
             for (int i = 0; i < chars.length; ) {
 
-                //если первый элемент начального массива = w
                 if (chars[i] == word[0] && i != chars.length - 1) {
 
-                    //проходим по массиву "word"
                     for (int j = 0; j < word.length; j++) {
 
-                        //если i элемент = j элементу в слове word
                         if (chars[i + j] == word[j] && i + j < chars.length) {
-                            //считаем количество совпадений
                             z++;
                         }
                     }
-                    //после прохождения по циклу ворд - начинаем проверять сколько раз встречалось
-                    //если количество совпадений равно количеству букв в слове word
+
                     if (z == word.length) {
-                        //обнуляем счетчик
                         z = 0;
-                        //проходимся по массиву letter
+
                         for (int l = 0; l < letter.length; l++) {
                             mass[p + l] = letter[l];
                         }
                         p = p + letter.length;
-                        //переходим к следующему i за словом word
                         i = i + word.length;
                     } else {
 
-                        if (p < mass.length && i < chars.length) {
+                        if (p < mass.length) {
                             mass[p] = chars[i];
                             p++;
                             i++;
                             z = 0;
                         }
-
                     }
                 } else {
-                    if (p < mass.length && i < chars.length) {
+                    if (p < mass.length) {
                         mass[p] = chars[i];
                         p++;
                         i++;
@@ -283,18 +143,11 @@ public class StringsAsArray {
                 }
             }
         }
+    }
 
-
-        for (int i = 0; i < chars.length; i++) {
-            System.out.print(chars[i]);
-        }
-
-        System.out.println();
-
-        for (int i = 0; i < mass.length; i++) {
-            System.out.print(mass[i]);
-        }
-
+    private static int lengthArray(char[] chars, char[] word, char[] letter, int count) {
+        int length;
+        return length = chars.length + (letter.length - word.length) * count;
     }
 
     public static void stringsAsArrayTask3() {
