d23371e4da96d8e6205e0189102be6f9a7f06b08
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-09-10 22:51:21 +0300

    void Class Test 1

--- src/com/svetilnik/Main.java
+++ src/com/svetilnik/Main.java
@@ -5,6 +5,7 @@ import com.svetilnik.basics.Branching;
 import com.svetilnik.basics.Cycles;
 import com.svetilnik.basics.LinearPrograms;
 import com.svetilnik.basics.StartBasics;
+import com.svetilnik.classes.Test1;
 import com.svetilnik.strings.StartStrings;
 
 import java.io.IOException;
@@ -15,7 +16,14 @@ public class Main {
 
 //        StartBasics.startBasics();
 //        StartAlgorithmization.startAlgorithmization();
-        StartStrings.startStrings();
+//        StartStrings.startStrings();
+
+        Test1 test1 = new Test1();
+        test1.print();
+        test1.setParam(1,2);
+        test1.print();
+        System.out.println("Sum = "+test1.sum());
+        System.out.println("Max = "+test1.ret());
     }
 
 }
