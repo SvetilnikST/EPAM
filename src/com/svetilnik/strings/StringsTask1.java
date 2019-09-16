0e683293bcf46947d901dcb223280634f9f4ebd4
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-09-16 16:53:55 +0300

    StringAsString - Pattern Matcher Start

--- src/com/svetilnik/strings/StringsTask1.java
+++ src/com/svetilnik/strings/StringsTask1.java
@@ -6,7 +6,10 @@ import java.util.regex.Pattern;
 public class StringsTask1 {
 
     private static final String REGEX = "\\bкот\\b";
-    private static final String INPUT = "кот кот кот котёл кот";
+    private static final String INPUT = "1 Думаю, что с технической точки зрения все достаточно понятно." +
+            "2 Но возникает логичный вопрос — зачем это вообще надо? С инкапсуляцией более-менее понятно, с наследованием — в принципе тоже." +
+            "3 Но вот этот механизм зачем, какое преимущество мы получим при использовании этой парадигмы?" +
+            "4 Для первого приближения! Рассмотрим наш пример графического приложения. В котором мы создавали свой компонент OvalComponent. ";
 
     public static void task1(){
         Pattern p = Pattern.compile(REGEX);
