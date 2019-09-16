5e6da51db24757e5f8d03fb00803e81cdeeefdc5
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-09-16 23:58:15 +0300

    void Class Test 2

--- src/com/svetilnik/Main.java
+++ src/com/svetilnik/Main.java
@@ -5,7 +5,8 @@ import com.svetilnik.basics.Branching;
 import com.svetilnik.basics.Cycles;
 import com.svetilnik.basics.LinearPrograms;
 import com.svetilnik.basics.StartBasics;
-import com.svetilnik.classes.Test1;
+
+import com.svetilnik.classes.StartTest;
 import com.svetilnik.strings.StartStrings;
 
 import java.io.IOException;
@@ -16,14 +17,8 @@ public class Main {
 
 //        StartBasics.startBasics();
 //        StartAlgorithmization.startAlgorithmization();
-        StartStrings.startStrings();
-
-//        Test1 test1 = new Test1();
-//        test1.print();
-//        test1.setParam(1,2);
-//        test1.print();
-//        System.out.println("Sum = "+test1.sum());
-//        System.out.println("Max = "+test1.ret());
+//        StartStrings.startStrings();
+        StartTest.startTest();
     }
 
 }
---
+++ src/com/svetilnik/classes/StartTest.java
@@ -0,0 +1,24 @@
+package com.svetilnik.classes;
+
+import com.svetilnik.classes.Test1;
+
+public class StartTest {
+
+    public static void startTest() {
+
+//        Test1 test1 = new Test1();
+//        test1.print();
+//        test1.setParam(1, 2);
+//        test1.print();
+//        System.out.println("Sum = " + test1.sum());
+//        System.out.println("Max = " + test1.ret());
+
+        Test2 test2 = new Test2();
+        System.out.println(test2.getA() + " ; " + test2.getB());
+        test2.setA(200);
+        test2.setB(300);
+        System.out.println(test2.getA()+";"+test2.getB());
+        Test2 test3 = new Test2(5,6);
+        System.out.println(test3.getA() + " ; " + test3.getB());
+    }
+}
