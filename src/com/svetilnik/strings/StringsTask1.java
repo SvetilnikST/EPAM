f862644ad8e00242f224235931205f2c25bdb232
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-09-16 23:43:43 +0300

    void Class Test 1

--- src/com/svetilnik/strings/StringsTask1.java
+++ src/com/svetilnik/strings/StringsTask1.java
@@ -5,23 +5,32 @@ import java.util.regex.Pattern;
 
 public class StringsTask1 {
 
-    private static final String REGEX = "\\bкот\\b";
+    private static final String REGEX = "[.!?]";
+    private static final String R = "^";
     private static final String INPUT = "1 Думаю, что с технической точки зрения все достаточно понятно." +
             "2 Но возникает логичный вопрос — зачем это вообще надо? С инкапсуляцией более-менее понятно, с наследованием — в принципе тоже." +
-            "3 Но вот этот механизм зачем, какое преимущество мы получим при использовании этой парадигмы?" +
+            "3 Но вот этот механизм зачем, какое преимущество мы получим при использовании этой парадигмы!" +
             "4 Для первого приближения! Рассмотрим наш пример графического приложения. В котором мы создавали свой компонент OvalComponent. ";
 
-    public static void task1(){
+    public static void task1() {
         Pattern p = Pattern.compile(REGEX);
-        Matcher m = p.matcher(INPUT);   // получение matcher объекта
-        int count = 0;
+        Pattern p2 = Pattern.compile(R);
 
-        while(m.find()) {
-            count++;
-            System.out.println("Номер вхождения: "+count);
-            System.out.println("Начальная позиция вхождения: "+m.start());
-            System.out.println("Конечная позиция вхождения: "+m.end());
+        Matcher m = p.matcher(INPUT);   // получение matcher объекта
+        Matcher m2 = p2.matcher(INPUT); //начало строки
+        int count = 0;
+        int t = 0;
+
+        if (m2.find()) {
+            t++;
+            while (m.find()) {
+                count++;
+//                System.out.println("Номер вхождения: " + count);
+            }
+            System.out.println("Кол-во предложений в " + t + " = " + count);
+            count = 0;
         }
+
 
     }
 }
