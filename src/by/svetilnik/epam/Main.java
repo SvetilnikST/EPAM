3af5901cadcb84f18725bfc1a999bec0889d0287
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-10-14 14:56:31 +0300

    Rename project

--- src/by/svetilnik/epam/Main.java
+++ src/by/svetilnik/epam/Main.java
@@ -2,6 +2,7 @@ package by.svetilnik.epam;
 
 import by.svetilnik.epam.b_algorithmization.StartAlgorithmization;
 import by.svetilnik.epam.c_strings.StartStrings;
+import by.svetilnik.epam.d_classes.aggregationAndComposition.StartAggregationAndComposition;
 
 
 public class Main {
@@ -9,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
 //        StartBasics.startBasics();
-        StartAlgorithmization.startAlgorithmization();
+//        StartAlgorithmization.startAlgorithmization();
 //        StartStrings.startStrings();
 //        StartSimpleClasses.startSimpleClasses();
 //        StartAggregationAndComposition.start();
