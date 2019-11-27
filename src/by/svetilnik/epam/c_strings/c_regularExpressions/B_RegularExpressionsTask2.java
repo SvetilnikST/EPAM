5921bbc20fdf8f5517d18dc1b7b4f74bde631f32
SvetilnikST <svetilnik.stezhkina@gmail.com>
2019-11-27 20:43:19 +0300

    strings task 2 - Done

--- src/by/svetilnik/epam/c_strings/c_regularExpressions/B_RegularExpressionsTask2.java
+++ src/by/svetilnik/epam/c_strings/c_regularExpressions/B_RegularExpressionsTask2.java
@@ -5,51 +5,42 @@ import java.util.regex.Pattern;
 
 public class B_RegularExpressionsTask2 {
 
-    private static final String xml = new String("<notes>\n" +
-            "    <note id =\"1\">\n" +
-            "        <to>Вася</to>\n" +
-            "        <from>Света</from>\n" +
-            "        <heading>Напоминание</heading>\n" +
-            "        <body>Позвони мне завтра!</body>\n" +
-            "    </note>\n" +
-            "\n" +
-            "    <note id =\"2\">\n" +
-            "        <to>Петя</to>\n" +
-            "        <from>Маша</from>\n" +
-            "        <heading>Важное напоминание</heading>\n" +
-            "        <body/>\n" +
-            "    </note>\n" +
-            "</notes>");
-
+    private static final String xml = new String("<notes><note id =\"1\"><to>Вася</to><from>Света</from><heading>Напоминание</heading><body>Позвони мне завтра!</body></note><note id =\"2\"><to>Петя</to><from>Маша</from><heading>Важное напоминание</heading><body/></note></notes>");
 
     public static void start() {
-        task2SortSentense(xml);
+        parserXML(xml);
     }
 
-    private static void task2SortSentense(String xml) {
+    private static void parserXML(String xml) {
+
         String openTag = "(?<openTag><[^/].*?>)";
         String closeTag = "(?<closeTag><[/].*?>)";
         String body = "(?<open><[^/].*?>)(?<body>.*)(?<close><[/].*?>)";
         String singleTag = "(?<singleTag><.*?[/]>)";
 
+        Pattern allString = Pattern.compile("<[^/].*?>(<[^/].*?>(<[^/].*?>.*<[/].*?>){1,}<[/].*?>){1}<[/].*?>$");
         Pattern pattern = Pattern.compile(body + "|" + openTag + "|" + closeTag + "|" + singleTag);
         Matcher matcher = pattern.matcher(xml);
 
-        //сделать проверку что строка в целом соответствует регулярке
-        while (matcher.find()) {
-            if (matcher.group("body") != null) {
-                System.out.println("Open: " + matcher.group("open") +
-                        "Body: " + matcher.group("body") +
-                        "Close: " + matcher.group("close")
-                );
 
-            } else if (matcher.group("openTag") != null) {
-                System.out.println("Open: " + matcher.group("openTag"));
-            } else if (matcher.group("closeTag") != null) {
-                System.out.println("Close: " + matcher.group("closeTag"));
-            } else if (matcher.group("singleTag") != null) {
-                System.out.println("Single: " + matcher.group("singleTag"));
+        Matcher matcher1 = allString.matcher(xml);
+        if (matcher1.matches()) {
+
+            while (matcher.find()) {
+                if (matcher.group("body") != null) {
+                    System.out.println("Open: " + matcher.group("open") +
+                            "Body: " + matcher.group("body") +
+                            "Close: " + matcher.group("close")
+                    );
+
+                } else if (matcher.group("openTag") != null) {
+                    System.out.println("Open: " + matcher.group("openTag"));
+                } else if (matcher.group("closeTag") != null) {
+                    System.out.println("Close: " + matcher.group("closeTag"));
+                } else if (matcher.group("singleTag") != null) {
+                    System.out.println("Single: " + matcher.group("singleTag"));
+                }
             }
-        }
+        } else System.out.println("Could not parse this line");
     }
 }
